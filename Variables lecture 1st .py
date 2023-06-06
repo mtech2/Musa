31728da7bbc9ae1b3db326c06ae5b73a5fce38ac
Muhammad Mustafa <127727557+musabhatti6878@users.noreply.github.com>
2023-06-07 00:54:53 +0500

    Python Variable Explanation 1st Lecture

---
+++ Variables lecture 1st .py
@@ -0,0 +1,96 @@
+# variables
+# int 1,2,3,4,5,5.....
+# float 1.2,100.34,
+# str "waleed","i am good boy"
+# chr "a","b,"c"
+# bool True,False
+
+# list[1,"waleed",12.3,[1,2,3,4,5]]
+# wa={
+#     "waleed":23
+# }
+
+# intgers
+# a=12
+# b=15
+# c=25
+# #a2+b2+2ab/a2+b2+3ab
+# print(2**5) #+,-,*,/,%,**
+
+
+
+#string
+# first_name="waleed"
+# last_name=" nasir"
+# num=10
+# print(type(num))
+# print(type(first_name))
+# num=float(num)
+# print(type(num))
+
+# sen="i am very Good boy. and you are Unable to be Like me"
+# #sen=sen.upper()
+# # sen=sen.lower()
+# # sen=sen.capitalize()
+# sen=sen[0::2]
+# print(sen)
+
+#list
+# names=["walled","ali","nam3","name4",1,2.4,[1,2,3,4,5,6,["a","b","c"]]]
+# # print(names[1][1])
+# names.append("hassan")
+# names.append("name7")
+# names.append(["z","y","x"])
+# names.insert(4,"waleed")
+# names.remove("walled")
+# names.reverse()
+# num=[5,8,9,2,1,3]
+# num.reverse()
+# del names[-1]
+# names.pop()
+# print(names)
+
+#dic
+# details={ "waleed":"pizza","hassan":"rice","mustafa":"orangs","usman":"zinger","shamil":"kheer"
+# }
+# print(details[0])
+
+# print("i\tam\ta\tgood\tboy","\ni am a bad boy")
+
+
+
+# num="23"
+
+
+# print(type(num))
+
+# num=int(num)
+
+# print(type(num))
+
+# num=float(num)
+
+# print(type(num))
+
+#userinput
+
+# ask1=input("enter your name: ")
+# print(ask1,"is a good name")12
+
+# num1=int(input("Enter first nuymber: "))
+# num2=input("enter second number: ")
+# print(int(num1)+int(num2))
+
+
+#import random as rm
+
+# val=random.randint(10,100)
+# print(val)
+# list_1=["red","bule","green","yellow","purple"]
+
+# # color=rm.choice(list_1)
+# # print(color)
+# rm.shuffle(list_1)
+# print(list_1)
+
+
